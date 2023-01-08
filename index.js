dc8522ecc823e2f4477559b4389c6107f355bfdf
chinmay81098 <chinmay81098@gmail>
2023-01-09 00:42:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+export function returnSum(a, b) {
+  return a + b;
+}
+
+export function returnProduct(a, b) {
+  return a * b;
+}
+
+export function returnDifference(a, b) {
+  return a - b;
+}
+
+export function returnQuotient(a, b) {
+  return a / b;
+}
+
